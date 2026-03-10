f5c903a20c5b5e12944460bc06035c016c471112
Yoge-2004 <yoge852004@gmail.com>
2026-03-10 13:59:58 +0530

    Track Windows app image with Git LFS

--- target/installer/LibraryApp/app/LibraryApp/bin/LibraryApp.bat
+++ target/installer/LibraryApp/app/LibraryApp/bin/LibraryApp.bat
@@ -1,4 +1,3 @@
-@echo off
-set JLINK_VM_OPTIONS=--enable-native-access=javafx.graphics
-set DIR=%~dp0
-"%DIR%\java" %JLINK_VM_OPTIONS% -m com.example.application/com.example.application.LibraryApp %*
+version https://git-lfs.github.com/spec/v1
+oid sha256:925fb18a0621e8d7698e41df56b32450b024c67ab3f318a7cfb702cd3a1c471c
+size 181
